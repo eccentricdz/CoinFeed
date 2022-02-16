54196c40953a2d49011eaaa45ee8ef9374a2db59
Rahul Agarwal <simplyrahul93@gmail.com>
2022-02-15 18:25:51 -0800

    Add icon and splash image.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import SourceArticles from "./components/SourceArticles";
 import SourceBuffet from "./components/SourceBuffet";
@@ -8,22 +8,25 @@ import SourceHeader from "./components/SourceHeader";
 import coinFeedStore, { CoinFeedStoreContext } from "./modules/CoinFeedStore";
 import Theme from "./modules/theme";
 import { fetchSources } from "./modules/utils";
+import AppLoading from "expo-app-loading";
 
 const App = observer(() => {
     useEffect(() => {
         fetchSources(coinFeedStore);
     }, []);
 
-    return (
-        <CoinFeedStoreContext.Provider value={coinFeedStore}>
-            <View style={styles.container}>
-                <SourceHeader></SourceHeader>
-                <SourceArticles></SourceArticles>
-                <SourceBuffet></SourceBuffet>
-                <StatusBar style="inverted" />
-            </View>
-        </CoinFeedStoreContext.Provider>
-    );
+    if (coinFeedStore.activeSource) {
+        return (
+            <CoinFeedStoreContext.Provider value={coinFeedStore}>
+                <View style={styles.container}>
+                    <SourceHeader></SourceHeader>
+                    <SourceArticles></SourceArticles>
+                    <SourceBuffet></SourceBuffet>
+                    <StatusBar style="inverted" />
+                </View>
+            </CoinFeedStoreContext.Provider>
+        );
+    } else return <AppLoading></AppLoading>;
 });
 
 const styles = StyleSheet.create({
